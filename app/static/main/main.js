29bc1269580b37ebb88d38c93716a64349853634
Ahmed Shahriar <47810596+shahriar0247@users.noreply.github.com>
2020-09-30 18:34:41 +0600

    Add files via upload

---
+++ app/static/main/main.js
@@ -0,0 +1,145 @@
+filepicker = document.getElementById('file-picker')
+uploadform = document.getElementById('upload-form')
+document.getElementById('uploadbtn').addEventListener('click', function () {
+    filepicker.click()
+})
+document.getElementById('uploadbtn2').addEventListener('click', function () {
+    filepicker.click()
+})
+
+
+filepicker.onchange = function () {
+    uploadform.submit()
+};
+
+document.getElementById('download-btn').addEventListener('click', function () {
+    document.getElementById('download_submit').click()
+})
+
+body = document.getElementsByTagName('body')[0]
+menu = document.getElementsByClassName('context-menu')[0]
+
+body.addEventListener('contextmenu', function (ev) {
+    ev.preventDefault();
+
+    menu.style.display = 'block'
+    menu.style.left = ev.pageX + 'px'
+    menu.style.top = ev.pageY + 'px'
+
+    if (filename3 == '[object HTMLParagraphElement]') {
+        delete1.style.display = 'none'
+    }
+    else {
+        delete1.style.display = 'block'
+    }
+
+
+    return false;
+}, false);
+
+// a = document.getElementsByClassName('a')[0]
+// c = document.getElementsByClassName('c')[0]
+// a1 = document.getElementsByClassName('a1')[0]
+// a2 = document.getElementsByClassName('a2')[0]
+// a3 = document.getElementsByClassName('a3')[0]
+// a.addEventListener('click', function () {
+//     clicking()
+//     nocont()
+// })
+// c.addEventListener('click', function () {
+//     clicking()
+//     nocont()
+// })
+// a1.addEventListener('click', function () {
+//     clicking()
+//     nocont()
+// })
+// a3.addEventListener('click', function () {
+//     clicking()
+//     nocont()
+// })
+
+document.addEventListener('click', function () {
+    clicking()
+    nocont()
+})
+
+menu1 = document.getElementById('menu1')
+new_folder = document.getElementById('new-folder')
+
+htmltopython = document.getElementById('htmltopython')
+new_folder.addEventListener('click', function () {
+    htmltopython.value = 'new-folder'
+    menu1.style.display = 'block'
+    document.getElementById('menu1btn1').innerText = 'Create Folder'
+    document.getElementById('menu1h2').innerText = 'Create a new folder'
+    document.getElementById('menu1p').innerText = 'Type the name of the folder you want to create and press on \'Create folder\''
+    document.getElementById('menu1text1').innerText = 'Folder name'
+    document.getElementById('menu1btn1').style.display = 'block'
+    document.getElementById('menu1cancel').style.display = 'block'
+    document.getElementById('menu1text2').style.display = 'block'
+
+
+
+    nocont()
+})
+
+
+menu1cancel = document.getElementById('menu1cancel')
+menu1cancel.addEventListener('click', function () {
+    menu1.style.display = 'none'
+})
+
+
+delete1 = document.getElementById('delete');
+
+
+
+delete1.addEventListener('click', function () {
+    htmltopython.value = 'delete%20,,@#' + filename3;
+    console.log(htmltopython.value)
+
+
+    menu1.style.display = 'block';
+    document.getElementById('menu1h2').innerText = 'Delete folder \'' + filename3 + '\'';
+    document.getElementById('menu1btn1').innerText = 'Delete Folder';
+    document.getElementById('menu1p').innerText = 'Are you sure you want to delete the folder?';
+    document.getElementById('menu1btn1').style.display = 'block';
+    document.getElementById('menu1cancel').style.display = 'block';
+    nocont()
+})
+
+document.getElementById('menu1btn1').addEventListener('click', function () {
+    document.getElementById('actionsubmit').click()
+})
+
+new_file = document.getElementById('new-file')
+
+
+new_file.addEventListener('click', function () {
+
+})
+
+function nocont() {
+    menu = document.getElementsByClassName('context-menu')[0]
+    menu.style.display = 'none'
+}
+
+
+function setgrid(num) {
+
+    folders = document.getElementsByClassName("folders")[0]
+    folders.classList.remove("foldersall")
+    folders.classList.remove("folders1")
+    folders.classList.remove("folders2")
+    folders.classList.remove("folders3")
+    folders.classList.remove("folders4")
+    folders.classList.remove("folders5")
+    if (num == "auto") {
+        folders.classList.add("foldersall")
+    }
+
+    else {
+        folders.classList.add("folders" + num)
+    }
+}
--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from app import app
 
 
 if __name__ == "__main__":
-    app.run(host='192.168.0.109', port=4200, debug=True)
+    app.run(host='127.0.0.1', port=4200, debug=True)
