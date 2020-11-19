de7c6418bbc035efb6dd8f7301242c5803ade333
Ahmed Shahriar <47810596+shahriar0247@users.noreply.github.com>
2020-11-20 02:42:50 +0600

    Add files via upload

--- app/static/login/login.js
+++ app/static/login/login.js
@@ -1,50 +1,116 @@
 
-               inputname = document.getElementsByClassName("inputname")
-            textbox = document.getElementsByClassName("textbox")
-            function add_hover_class(i){
-                    inputname[i].classList.add("inputnamehover")
-                }
+inputname = document.getElementsByClassName("inputname")
+textbox = document.getElementsByClassName("textbox")
+function add_hover_class(i) {
+   
+    inputname[i].classList.add("inputnamehover")
+}
 
-            function remove_hover_class(i){
-                    inputname[i].classList.remove("inputnamehover")
-                }
-            function TestOnTextChange(input){
-                for (let i = 0; i < textbox.length ; i++){
-                    if (textbox[i] == input){
-                        add_hover_class(i)
-                      
-                    }
-                }
-            }
+function remove_hover_class(i) {
+    inputname[i].classList.remove("inputnamehover")
+}
+function TestOnTextChange(input) {
+    for (let i = 0; i < textbox.length; i++) {
+        if (textbox[i] == input) {
+            add_hover_class(i)
+
+        }
+    }
+}
+var hoveron = [false, false]
+var focuson = [false, false]
+
+for (let i = 0; i < textbox.length; i++) {
+
+
+    if (textbox[i].value == "") {
+        remove_hover_class(i)
+    }
+    else {
+        add_hover_class(i)
+    }
     
-            for (let i = 0; i < textbox.length; i++) {
-            
-             
-                    if (textbox[i].value == ""){
-                   
-                    }
-                    else{
-                        add_hover_class(i)
-                    }
-                
-                textbox[i].addEventListener("mouseover",function(){
-                   add_hover_class(i)
-                })
-                inputname[i].addEventListener("mouseover",function(){
-                   add_hover_class(i)
-                })
-
-                textbox[i].addEventListener("mouseout",function(){
-                    if (textbox[i].value == ""){
-                    remove_hover_class(i)
-                }
-                })
-                inputname[i].addEventListener("mouseout",function(){
-                    if (textbox[i].value == ""){
-                    remove_hover_class(i)}
-                })
-          
-            }
-
-
+    textbox[i].addEventListener("mouseover", function () {
         
+        hoveron[i] = true 
+      
+    })
+    inputname[i].addEventListener("mouseover", function () {
+        (hoveron[i] = true) 
+    })
+
+    textbox[i].addEventListener("mouseout", function () {
+     
+            (hoveron[i] = false) 
+        
+    })
+    inputname[i].addEventListener("mouseout", function () {
+     
+            (hoveron[i] = false) 
+        
+    })
+    textbox[i].addEventListener("focus", function () {
+        
+        focuson[i] = true 
+      
+    })
+    inputname[i].addEventListener("focus", function () {
+        (focuson[i] = true) 
+    })
+
+    textbox[i].addEventListener("blur", function () {
+     
+            (focuson[i] = false) 
+        
+    })
+    inputname[i].addEventListener("blur", function () {
+     
+            (focuson[i] = false) 
+        
+    })
+
+}
+
+function if_text_in_input() {
+    for (let i = 0; i < textbox.length; i++) {
+        
+    if (hoveron[i] == true){
+        add_hover_class(i)
+    }
+
+    if (focuson[i] == true){
+        add_hover_class(i)
+    }
+    if (textbox[i].value != "")
+    {
+        add_hover_class(i)
+    }
+    if (hoveron[i] == false && textbox[i].value == false && focuson[i] == false)
+{
+    remove_hover_class(i)
+}
+    }
+}
+
+function async_loop() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('resolved');
+        }, 100);
+    });
+}
+
+async function asyncCall() {
+
+    while (true) {
+        const result = await async_loop();
+        if_text_in_input()
+        console.log("lol")
+    }
+
+}
+
+asyncCall();
+
+
+
